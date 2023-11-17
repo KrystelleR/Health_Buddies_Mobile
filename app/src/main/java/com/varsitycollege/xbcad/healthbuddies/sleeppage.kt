645f4bef1304744d6d348243c23996e2188cf9e9
Krystelle Rupnarain <104322527+KrystelleR@users.noreply.github.com>
2023-11-17 12:23:58 +0200

    breathing pages

--- app/src/main/java/com/varsitycollege/xbcad/healthbuddies/sleeppage.kt
+++ app/src/main/java/com/varsitycollege/xbcad/healthbuddies/sleeppage.kt
@@ -1,11 +1,26 @@
 package com.varsitycollege.xbcad.healthbuddies
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class sleeppage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sleeppage)
+
+        val melodycard = findViewById<CardView>(R.id.melodiescard)
+        val breathingcard = findViewById<CardView>(R.id.breathingcard)
+
+        melodycard.setOnClickListener {
+            val intent = Intent(this, melodiespage::class.java)
+            startActivity(intent)
+        }
+
+        breathingcard.setOnClickListener {
+            val intent = Intent(this, breathingpage::class.java)
+            startActivity(intent)
+        }
     }
 }
